dd95f7ea92d8a6b4549e18e6f1d2be0d5600b815
Yasas Mahima <yasasymahima@gmail.com>
2020-07-28 11:47:36 +0530

    Update App

--- Recommendation App/src/main/scala/sampleApp1/MovieRecommendations.scala
+++ Recommendation App/src/main/scala/sampleApp1/MovieRecommendations.scala
@@ -37,7 +37,7 @@ object MovieRecommendations {
     ((id1,id2),(rating1,rating2))
   }
 
-// Method to make CosineSimilarityMatrix
+// Method to make CosineSimilarityMatrix - Find Similarity between two vectors
   def getCosineSimilarityMatrix(pairWiseMovieRatings: Iterable[(Double, Double)]): (Double,Int) = {
     var numberOfMoviePairs: Int =0
     var sumXX: Double =0.0
@@ -54,6 +54,7 @@ object MovieRecommendations {
       numberOfMoviePairs+=1
     }
 
+//    CosQ = A.B / |A||B|
     val numerator: Double =sumXY
     val denominator: Double =Math.sqrt(sumXX)*Math.sqrt(sumYY)
     val result: Double =numerator/denominator
@@ -69,29 +70,37 @@ object MovieRecommendations {
     code_encode.onMalformedInput(CodingErrorAction.REPLACE)
     code_encode.onUnmappableCharacter(CodingErrorAction.REPLACE)
 
-    val idAndNameMapped: Map[Int, String] = Source.fromFile("D:\\Zone24x7\\Recommendation App\\src\\main\\resources\\u.item").getLines().map(line =>{
-      val lineArr=line.split('|')
-      (lineArr.head.toInt,lineArr(1))
+    val movieIdAndName: Map[Int, String] = Source.fromFile("D:\\Zone24x7\\Recommendation App\\src\\main\\resources\\u.item").getLines().map(line =>{
+      val lines=line.split('|')
+      (lines.head.toInt,lines(1))
     }).toMap[Int,String]
 
-    idAndNameMapped
+    movieIdAndName
+  }
+
+  def filterAccordingToThreshold(moviePairAndScore: ((Int, Int), (Double, Int)),movieId:Int):Boolean= {
+
+    val scoreThreshold: Double = 0.97 //Calculated for data set (Cosine value)(1 means exactly the same movie)
+    val numberOfOccurences: Double = 50.0 // Depends on data set (Number of similar pairs)
+
+    val moviePair: (Int, Int) = moviePairAndScore._1
+    val ratingAndNumOfPairs: (Double, Int) = moviePairAndScore._2
+    (moviePair._1 == movieId || moviePair._2 == movieId) && ratingAndNumOfPairs._1 > scoreThreshold && ratingAndNumOfPairs._2 > numberOfOccurences
+
   }
 
 //  Def method to get top 10 related movie to given
-  def suggestTop10Movies(moviesAndSimilarityScore: RDD[((Int, Int), (Double, Int))],args:Array[String]): Unit = {
+  def getTop10Movies(moviesAndSimilarityScore: RDD[((Int, Int), (Double, Int))], movieId:Int): Unit = {
 
-    val scoreThreshold: Double = 0.97  //Assumed value
-    val coOccurenceThreshold: Double = 50.0 //Assumed value
+    val scoreThreshold: Double = 0.97
+    val coOccurenceThreshold: Double = 50.0
 
-    val movieId: Int =args.head.toInt
 
     val moviePairsFilteredAccordingToThreshold: RDD[((Int, Int), (Double, Int))] = moviesAndSimilarityScore.filter((moviePairAndScore: ((Int, Int), (Double, Int))) => {
-      val moviePair: (Int, Int) =moviePairAndScore._1
-      val ratingAndNumOfPairs: (Double, Int) =moviePairAndScore._2
-      (moviePair._1==movieId || moviePair._2==movieId) && ratingAndNumOfPairs._1>scoreThreshold && ratingAndNumOfPairs._2>coOccurenceThreshold
+     filterAccordingToThreshold(moviePairAndScore,movieId)
     })
 
-    val first10MoviesAndTheirScores: Array[((Int, Int), (Double, Int))] =moviePairsFilteredAccordingToThreshold.take(10)
+    val first10MoviesAndTheirScores: Array[((Int, Int), (Double, Int))] =moviePairsFilteredAccordingToThreshold.sortByKey().take(10)
 
     val idAndMovieNames=mapMovieIdAndName()
 
@@ -104,6 +113,7 @@ object MovieRecommendations {
       val movie1: Int =moviePairAndScore._1._1
       val movie2: Int =moviePairAndScore._1._2
       var suggestedMovie: Int =movie2
+
       if(movie2==movieId) {
         suggestedMovie = movie1
       }
@@ -132,13 +142,10 @@ object MovieRecommendations {
 //    Ger Similarity Score for each movie pairs using CosineSimilarity Matrix
     val cosineSimilarityScoreForEachPair: RDD[((Int, Int), (Double, Int))] =groupPairOfMovies.mapValues(getCosineSimilarityMatrix)
 
-    if(args.length>0)
+      print("Enter the Movie Id : ")
+      val movieId: Int = scala.io.StdIn.readLine.toInt
 
-      suggestTop10Movies(cosineSimilarityScoreForEachPair,args)
-
-    else
-      println("Please give movie Id as command line argument")
-
+      getTop10Movies(cosineSimilarityScoreForEachPair,movieId)
 
   }
 
